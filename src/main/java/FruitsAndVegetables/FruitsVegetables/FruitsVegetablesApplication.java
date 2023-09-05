a39ea046287fd0ff54f6dc4fcaad7a483f4e0973
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-09-05 19:06:55 +0300

    Initial commit

---
+++ src/main/java/FruitsAndVegetables/FruitsVegetables/FruitsVegetablesApplication.java
@@ -0,0 +1,13 @@
+package FruitsAndVegetables.FruitsVegetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsVegetablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsVegetablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FruitsAndVegetables/FruitsVegetables/FruitsVegetablesApplicationTests.java
@@ -0,0 +1,13 @@
+package FruitsAndVegetables.FruitsVegetables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsVegetablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
